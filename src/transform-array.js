d86b7a18523e3d3008284e292a4433e118642201
Anton <lipan48@gmail.com>
2023-12-21 10:06:21 +0300

    feat: complete add solutions

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  let newArr = [];
+  let skipNext = false;
+
+  arr.forEach((el, i) => {
+    if (skipNext) {
+      skipNext = false;
+      return;
+    }
+
+    switch(el) {
+      case '--discard-next':
+        skipNext = true;
+        break;
+      case '--discard-prev':
+        if (i !== 0 && arr[i - 2] !== '--discard-next') newArr.pop();
+        break;
+      case '--double-next':
+        if (i !== arr.length - 1) newArr.push(arr[i + 1]);
+        break;
+      case '--double-prev':
+        if (i !== 0 && arr[i - 2] !== '--discard-next') {
+          newArr.push(newArr[newArr.length - 1]);
+        }
+        break;
+      default:
+        newArr.push(el);
+        break;
+    }
+  });
+
+  return newArr;
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,71 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.alphaLength = 26;
+    this.startPos = 65;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  config(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    if (message.length > key.length) {
+      key = key.repeat(Math.ceil(message.length / key.length));
+    }
+    this.message = message;
+    this.key = key;
+  }
+
+  encrypt(message, key) {
+    this.config(message, key);
+
+    let result = '';
+    let shift = 0;
+
+    for (let i = 0; i < this.message.length; i += 1) {
+      const char = this.message.charCodeAt(i) - this.startPos;
+      const shiftChar = this.key.charCodeAt(i - shift) - this.startPos;
+
+      if (char < 0 || char > this.alphaLength) {
+        result += this.message[i];
+        shift += 1;
+      } else {
+        result += String.fromCharCode(
+          ((char + shiftChar) % this.alphaLength) + this.startPos
+        )
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(message, key) {
+    this.config(message, key);
+
+    let result = '';
+    let shift = 0;
+
+    for (let i = 0; i < this.message.length; i += 1) {
+      const char = this.message.charCodeAt(i) - this.startPos;
+      const shiftChar = this.key.charCodeAt(i - shift) - this.startPos;
+
+      if (char < 0 || char > this.alphaLength) {
+        result += this.message[i];
+        shift += 1;
+      } else {
+        result += String.fromCharCode(
+          ((char - shiftChar + this.alphaLength) % this.alphaLength) + this.startPos
+        )
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
   }
 }
 
